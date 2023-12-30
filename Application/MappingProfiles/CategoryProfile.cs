90a9882841bf49a6b4940962ad64f92cca5468cb
UsikovKonstantin <usikov-kostya@mail.ru>
2023-12-30 09:29:18 +0300

    Добавил AutoMapper и MediatR

---
+++ Application/MappingProfiles/CategoryProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Domain.Models;
+
+namespace Application.MappingProfiles;
+
+public class CategoryProfile : Profile
+{
+    public CategoryProfile()
+    {
+        // TODO: заменить первый класс на CategoryDto
+        CreateMap<Category, Category>().ReverseMap();
+    }
+}
